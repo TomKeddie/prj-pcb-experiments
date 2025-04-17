6aac917a92d9970b16da36490a05a07fa8b45508
Tom Keddie <github@bronwenandtom.com>
2025-04-16 19:54:06 -0700

    2025-usb-pd-plank: new design
    Prior to dropping BGA for FETs

---
+++ 2025-usb-pd-plank/kicad/kicad_jlc_rotate.py
@@ -0,0 +1,49 @@
+import pcbnew
+import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pcbfile", required=True, help="File name of kicad PCB file")
+parser.add_argument("--posinfile", required=True, help="File name of kicad POS input file")
+parser.add_argument("--posoutfile", required=True, help="File name of kicad POS output file")
+args = parser.parse_args()
+
+try:
+    board = pcbnew.LoadBoard(args.pcbfile)
+except:
+    print("Error loading {}".format(args.pcbfile))
+    sys.exit(-1)
+
+footprints = board.Footprints()
+
+#for footprint in footprints:
+#    print(footprint.GetReference())
+
+try:
+    posinfile = open(args.posinfile, newline='')
+    try:
+        posoutfile = open(args.posoutfile, 'w', newline='')
+        firstline=True
+        for line in posinfile:
+            if firstline:
+                firstline = False
+                posoutfile.write(line)
+            else:
+                row = line.split(",")
+                row[1] = float(row[1])
+                row[2] = float(row[2])
+                row[4] = float(row[4])
+                footprint = board.FindFootprintByReference(row[0].strip('"'))
+                if footprint:
+                    jlcpcb_rotation_delta = footprint.GetFieldByName("JLCPCB_ROTATION_DELTA")
+                    if jlcpcb_rotation_delta:
+                        row[4] += int(float(jlcpcb_rotation_delta.GetText()))
+                        if row[4] > 359:
+                            row[4] -= 360
+                        elif row[4] < -359:
+                            row[4] += 260
+                posoutfile.write("{},{:.06f},{:.06f},{},{:.06f}\n".format(row[0], row[1], row[2], row[3], row[4]))
+    except:
+        print("Error opening {}".format(args.posoutfile))
+except:
+    print("Error opening {}".format(args.posinfile))
